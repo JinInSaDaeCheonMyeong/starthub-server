05f653061925b98f778f2f8f57a103bfbc6db5d6
bestdevmgp <pmg3858@icloud.com>
2025-04-03 20:17:41 +0900

    feat: auth

---
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/security/token/core/TokenValidator.kt
@@ -0,0 +1,36 @@
+package com.jininsadaecheonmyeong.starthubserver.global.security.token.core
+
+import com.jininsadaecheonmyeong.starthubserver.global.security.token.enumeration.TokenType
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.security.SignatureException
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class TokenValidator(
+    private val parser: TokenParser,
+) {
+    fun validateAll(token: String, tokenType: TokenType) {
+        validate(token)
+        validateType(token, tokenType)
+    }
+
+    fun validateType(token: String, tokenType: TokenType) {
+        if (parser.findType(token) != tokenType) throw RuntimeException("token mismatch")
+    }
+
+    fun validate(token: String) {
+        try {
+            parser.findType(token)
+            if (
+                parser.findExpiration(token).before(Date())
+            ) throw RuntimeException("토큰 만료됨")
+        } catch (e: ExpiredJwtException) {
+            throw RuntimeException("토큰 만료됨")
+        } catch (e: SignatureException) {
+            throw RuntimeException("유효하지 않은 토큰 시그니처")
+        } catch (e: Exception) {
+            throw RuntimeException("알 수 없는 토큰")
+        }
+    }
+}
