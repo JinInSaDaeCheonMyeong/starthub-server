c3728ab42fe67100e60e257eea42fc1fbcc56b05
dongwhi09 <dongwhi09@naver.com>
2025-04-10 19:27:57 +0900

    feat: implement refresh token management using Redis

---
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/security/configuration/RedisConfig.kt
@@ -0,0 +1,37 @@
+package com.jininsadaecheonmyeong.starthubserver.global.security.configuration
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val configuration = RedisStandaloneConfiguration().apply {
+            hostName = redisProperties.host
+            port = redisProperties.port
+            password = RedisPassword.of(redisProperties.password)
+        }
+
+        return LettuceConnectionFactory(configuration)
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        return RedisTemplate<String, String>().apply {
+            this.connectionFactory = redisConnectionFactory
+            this.keySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
+    }
+}
