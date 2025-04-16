2a95e249366d5887b24182b2170d7c7b7587aaf1
dongwhi09 <dongwhi09@naver.com>
2025-04-16 20:41:48 +0900

    feat: add email validation and exception handler

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/data/UserRequest.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/data/UserRequest.kt
@@ -2,8 +2,10 @@ package com.jininsadaecheonmyeong.starthubserver.domain.user.data
 
 import com.jininsadaecheonmyeong.starthubserver.domain.user.entity.User
 import com.jininsadaecheonmyeong.starthubserver.domain.user.enumeration.AuthProvider
+import jakarta.validation.constraints.Email
 
 data class UserRequest(
+    @field:Email(message = "올바르지 않은 이메일 형식")
     val email: String,
     val password: String,
 ) {
