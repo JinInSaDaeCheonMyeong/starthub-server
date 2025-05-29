4bcbd54b00612689ae7d30b489f56cf4ed9f8693
bestdevmgp <im@parkmingyu.com>
2025-05-29 20:49:33 +0900

    feat: password normalization

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/presentation/UserController.kt
@@ -6,6 +6,7 @@ import com.jininsadaecheonmyeong.starthubserver.domain.user.service.UserService
 import com.jininsadaecheonmyeong.starthubserver.global.common.BaseResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,7 +20,7 @@ class UserController(
 ) {
     @Operation(summary = "회원가입", description = "이메일 인증을 먼저 해야합니다.")
     @PostMapping("/sign-up")
-    fun signUp(@RequestBody request: UserRequest)
+    fun signUp(@RequestBody @Valid request: UserRequest)
         = BaseResponse.created(userService.signUp(request), "회원가입 성공")
 
     @Operation(summary = "로그인", description = "사용자 로그인을 처리하고 JWT 토큰을 반환합니다.")
