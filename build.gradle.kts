0a38e16697d8c23528dec05ca074cd3a53b9033d
bestdevmgp <im@parkmingyu.com>
2025-05-29 20:49:33 +0900

    feat: password normalization

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     // basic
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // email
