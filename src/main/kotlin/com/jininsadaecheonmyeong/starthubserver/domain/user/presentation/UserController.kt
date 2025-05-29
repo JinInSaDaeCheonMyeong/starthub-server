d6fe9d828211f691fbb94504f7327e833302b499
bestdevmgp <im@parkmingyu.com>
2025-05-29 23:30:00 +0900

    merge: conflict

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/presentation/UserController.kt
@@ -4,9 +4,6 @@ import com.jininsadaecheonmyeong.starthubserver.domain.user.data.RefreshRequest
 import com.jininsadaecheonmyeong.starthubserver.domain.user.data.UserRequest
 import com.jininsadaecheonmyeong.starthubserver.domain.user.service.UserService
 import com.jininsadaecheonmyeong.starthubserver.global.common.BaseResponse
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,22 +11,25 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/user")
-@Tag(name = "유저", description = "사용자 관련 API")
 class UserController(
     private val userService: UserService,
-) {
-    @Operation(summary = "회원가입", description = "이메일 인증을 먼저 해야합니다.")
+) : UserDocs {
     @PostMapping("/sign-up")
-    fun signUp(@RequestBody @Valid request: UserRequest)
-        = BaseResponse.created(userService.signUp(request), "회원가입 성공")
+    override fun signUp(request: UserRequest)
+            = BaseResponse.of(userService.signUp(request), "회원가입 성공")
 
-    @Operation(summary = "로그인", description = "사용자 로그인을 처리하고 JWT 토큰을 반환합니다.")
     @PostMapping("/sign-in")
-    fun signIn(@RequestBody request: UserRequest)
-        = BaseResponse.ok(userService.signIn(request), "로그인 성공")
+    override fun signIn(request: UserRequest)
+            = BaseResponse.of(userService.signIn(request), "로그인 성공")
 
-    @Operation(summary = "토큰 재발급", description = "리프레시 토큰을 사용하여 새로운 액세스 토큰을 발급받습니다.")
     @PostMapping("/reissue")
-    fun reissue(@RequestBody request: RefreshRequest)
-        = BaseResponse.ok(userService.reissue(request), "토큰 재발급 성공")
+    override fun reissue(request: RefreshRequest)
+            = BaseResponse.of(userService.reissue(request), "토큰 재발급 성공")
+
+    @PatchMapping("/profile")
+    fun updateUserProfile(@RequestBody request: UpdateUserProfileRequest): ResponseEntity<BaseResponse<Unit>> {
+        val currentUser = UserAuthenticationHolder.current()
+        userService.updateUserProfile(currentUser, request.username, request.interests)
+        return BaseResponse.of("유저 프로필 설정 성공")
+    }
 }
