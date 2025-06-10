ace41cbd7ecdca93095ecf317e6d56f0388b8e12
dongwhi09 <dongwhi09@naver.com>
2025-06-11 07:52:27 +0900

    feat: add InvalidStateException for OAuth state mismatch #20

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/enums/InterestType.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/user/enums/InterestType.kt
@@ -3,8 +3,7 @@ package com.jininsadaecheonmyeong.starthubserver.domain.user.enums
 enum class InterestType(val displayName: String) {
     CONTENT_MEDIA("콘텐츠/미디어"),
     FINTECH("핀테크"),
-    HEALTHCARE("헬스케어"),
-    BIO("바이오"),
+    HEALTHCARE_BIO("헬스케어/바이오"),
     EDUCATION_EDUTECH("교육/에듀테크"),
     IT_SOFTWARE("IT/소프트웨어"),
     ECOMMERCE("전자상거래"),
