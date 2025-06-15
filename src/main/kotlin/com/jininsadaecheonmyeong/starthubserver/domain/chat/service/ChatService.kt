b4e062db97607abbe4dcac9422e9e107bc9e4091
dongwhi09 <dongwhi09@naver.com>
2025-06-15 19:42:56 +0900

    feat: add customException and change message

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/chat/service/ChatService.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/chat/service/ChatService.kt
@@ -5,8 +5,10 @@ import com.jininsadaecheonmyeong.starthubserver.domain.chat.data.ChatRoomRespons
 import com.jininsadaecheonmyeong.starthubserver.domain.chat.data.CreateChatMessageDto
 import com.jininsadaecheonmyeong.starthubserver.domain.chat.entity.ChatMessage
 import com.jininsadaecheonmyeong.starthubserver.domain.chat.entity.ChatRoom
+import com.jininsadaecheonmyeong.starthubserver.domain.chat.exception.ChatRoomNotFoundException
 import com.jininsadaecheonmyeong.starthubserver.domain.chat.repository.ChatMessageRepository
 import com.jininsadaecheonmyeong.starthubserver.domain.chat.repository.ChatRoomRepository
+import com.jininsadaecheonmyeong.starthubserver.domain.user.exception.UserNotFoundException
 import com.jininsadaecheonmyeong.starthubserver.domain.user.repository.UserRepository
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
@@ -23,8 +25,8 @@ class ChatService(
 ) {
     @Transactional
     fun getOrCreateChatRoom(user1Id: UUID, user2Id: UUID): ChatRoomResponse {
-        val user1 = userRepository.findById(user1Id).orElseThrow { IllegalArgumentException("User1 not found") }
-        val user2 = userRepository.findById(user2Id).orElseThrow { IllegalArgumentException("User2 not found") }
+        val user1 = userRepository.findById(user1Id).orElseThrow { UserNotFoundException("찾을 수 없는 유저") }
+        val user2 = userRepository.findById(user2Id).orElseThrow { UserNotFoundException("찾을 수 없는 유저") }
 
         val existingRoom = chatRoomRepository.findByUser1AndUser2(user1, user2)
             ?: chatRoomRepository.findByUser2AndUser1(user1, user2)
@@ -37,9 +39,9 @@ class ChatService(
     @Transactional
     fun saveAndSendMessage(createChatMessageDto: CreateChatMessageDto): ChatMessageResponse {
         val room = chatRoomRepository.findById(createChatMessageDto.roomId)
-            .orElseThrow { IllegalArgumentException("ChatRoom not found") }
+            .orElseThrow { ChatRoomNotFoundException("찾을 수 없는 채팅방") }
         val sender = userRepository.findById(createChatMessageDto.senderId)
-            .orElseThrow { IllegalArgumentException("Sender not found") }
+            .orElseThrow { UserNotFoundException("찾을 수 없는 유저") }
 
         val chatMessage = ChatMessage(
             room = room,
