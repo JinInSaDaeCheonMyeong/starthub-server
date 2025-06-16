893a3e197359b36cddcf3b87679c185257f98011
bestdevmgp <me@mingyu.dev>
2025-06-16 12:02:14 +0900

    chore: add profile-image on user

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/company/data/request/UpdateCompanyProfileRequest.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/company/data/request/UpdateCompanyProfileRequest.kt
@@ -6,7 +6,8 @@ data class UpdateCompanyProfileRequest(
     val companyDescription: String? = null,
     val businessDescription: String? = null,
     val pageUrl: String? = null,
-    @field:Email val email: String? = null,
+    @field:Email
+    val email: String? = null,
     val tel: String? = null,
     val address: String? = null,
     val employeeCount: Int? = null,
