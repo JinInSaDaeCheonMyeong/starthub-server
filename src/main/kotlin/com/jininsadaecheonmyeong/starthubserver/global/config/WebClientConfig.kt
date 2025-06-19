5801be200569b83e9d28fe0f1b2d85ef76a243d7
bestdevmgp <me@mingyu.dev>
2025-06-19 17:05:04 +0900

    set discord logback

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/config/WebClientConfig.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/config/WebClientConfig.kt
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
 
-
 @Configuration
 class WebClientConfig {
+
     @Bean
-    fun webClientBuilder(): WebClient.Builder {
+    fun webClient(): WebClient {
         return WebClient.builder()
+            .codecs { configurer ->
+                configurer.defaultCodecs().maxInMemorySize(2 * 1024 * 1024) // 2MB
+            }
+            .build()
     }
 }
