d1afa06898cb4ee88cc210a24e2c3cd475b915f1
dongwhi09 <dongwhi09@naver.com>
2025-07-17 10:31:31 +0900

    chore: delete unused annotation

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/recruit/presentation/RecruitController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/recruit/presentation/RecruitController.kt
@@ -7,7 +7,6 @@ import com.jininsadaecheonmyeong.starthubserver.domain.recruit.data.response.Rec
 import com.jininsadaecheonmyeong.starthubserver.domain.recruit.docs.RecruitDocs
 import com.jininsadaecheonmyeong.starthubserver.domain.recruit.service.RecruitService
 import com.jininsadaecheonmyeong.starthubserver.global.common.BaseResponse
-import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -15,19 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/recruits")
+@RequestMapping("/recruits")
 class RecruitController(
     private val recruitService: RecruitService,
 ) : RecruitDocs {
     @PostMapping
     override fun createRecruit(
-        @RequestBody @Valid request: CreateRecruitRequest,
+        request: CreateRecruitRequest,
     ): ResponseEntity<BaseResponse<RecruitResponse>> {
         return BaseResponse.of(recruitService.createRecruit(request), "채용 공고 생성 성공")
     }
@@ -35,7 +33,7 @@ class RecruitController(
     @PatchMapping("/{id}")
     override fun updateRecruit(
         @PathVariable id: Long,
-        @RequestBody @Valid request: UpdateRecruitRequest,
+        request: UpdateRecruitRequest,
     ): ResponseEntity<BaseResponse<RecruitResponse>> {
         return BaseResponse.of(recruitService.updateRecruit(id, request), "채용 공고 수정 성공")
     }
