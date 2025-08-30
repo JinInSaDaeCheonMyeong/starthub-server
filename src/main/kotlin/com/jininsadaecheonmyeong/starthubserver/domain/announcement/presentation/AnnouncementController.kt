4102632862b946785a197373988d6fb55c82fc43
dongwhi09 <dongwhi09@naver.com>
2025-08-30 17:08:54 +0900

    chore: Delete unused import

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/announcement/presentation/AnnouncementController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/announcement/presentation/AnnouncementController.kt
@@ -10,9 +10,7 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
