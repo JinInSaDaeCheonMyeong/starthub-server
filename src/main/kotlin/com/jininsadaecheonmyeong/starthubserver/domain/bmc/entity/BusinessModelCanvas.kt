5ca743d040ca08fba0fcb73a28e2770c8aaf7bf1
bestdevmgp <me@mingyu.dev>
2025-09-17 20:39:43 +0900

    chore: convention

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/bmc/entity/BusinessModelCanvas.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/bmc/entity/BusinessModelCanvas.kt
@@ -2,15 +2,15 @@ package com.jininsadaecheonmyeong.starthubserver.domain.bmc.entity
 
 import com.jininsadaecheonmyeong.starthubserver.domain.user.entity.User
 import com.jininsadaecheonmyeong.starthubserver.global.common.BaseEntity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.JoinColumn
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "business_model_canvas")
