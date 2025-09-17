d390aefbfa036ee4cfe8842d029b4cdbc1c49d29
bestdevmgp <me@mingyu.dev>
2025-09-17 20:37:21 +0900

    fix: convention

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/bmc/entity/BusinessModelCanvas.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/bmc/entity/BusinessModelCanvas.kt
@@ -2,7 +2,15 @@ package com.jininsadaecheonmyeong.starthubserver.domain.bmc.entity
 
 import com.jininsadaecheonmyeong.starthubserver.domain.user.entity.User
 import com.jininsadaecheonmyeong.starthubserver.global.common.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 
 @Entity
 @Table(name = "business_model_canvas")
