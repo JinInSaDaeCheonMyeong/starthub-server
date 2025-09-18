8d938fe2fd7a7fa2e90c992673f9d0def35f404f
bestdevmgp <me@mingyu.dev>
2025-09-18 21:43:51 +0900

    refactor: fix redis key

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/security/token/core/TokenRedisService.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/global/security/token/core/TokenRedisService.kt
@@ -14,14 +14,18 @@ class TokenRedisService(
         email: String,
         refreshToken: String,
     ) {
-        redisTemplate.opsForValue().set("refreshToken:$email", refreshToken, properties.refresh, TimeUnit.MILLISECONDS)
+        redisTemplate.opsForValue().set("$KEY_PREFIX:$email", refreshToken, properties.refresh, TimeUnit.MILLISECONDS)
     }
 
     fun findByEmail(email: String): String? {
-        return redisTemplate.opsForValue()["refreshToken:$email"]
+        return redisTemplate.opsForValue()["$KEY_PREFIX:$email"]
     }
 
     fun deleteRefreshToken(email: String) {
-        redisTemplate.delete("refreshToken:$email")
+        redisTemplate.delete("$KEY_PREFIX:$email")
+    }
+
+    companion object {
+        private const val KEY_PREFIX = "refresh_token:"
     }
 }
