97b485af2d5b4901bce3dbadaa6e01dda87c3fd5
dongwhi09 <dongwhi09@naver.com>
2025-10-08 19:33:57 +0900

    refactor: Use BaseResponse.of in ScheduleController

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/schedule/docs/ScheduleDocs.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/schedule/docs/ScheduleDocs.kt
@@ -6,6 +6,7 @@ import com.jininsadaecheonmyeong.starthubserver.global.common.BaseResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import java.time.LocalDate
@@ -15,20 +16,20 @@ interface ScheduleDocs {
     @Operation(summary = "일정 생성", description = "새로운 일정을 생성합니다.")
     fun createSchedule(
         @RequestBody request: ScheduleRequest,
-    ): BaseResponse<Unit>
+    ): ResponseEntity<BaseResponse<Unit>>
 
     @Operation(summary = "일정 삭제", description = "일정을 삭제합니다.")
     fun deleteSchedule(
         @Parameter(description = "삭제할 공고 ID") @RequestParam announcementId: Long,
-    ): BaseResponse<Unit>
+    ): ResponseEntity<BaseResponse<Unit>>
 
     @Operation(summary = "월별 일정 조회", description = "특정 월의 일정 목록을 조회합니다.")
     fun getSchedulesByMonth(
         @Parameter(description = "조회할 월의 아무 날짜") @RequestParam date: LocalDate,
-    ): BaseResponse<List<ScheduleResponse>>
+    ): ResponseEntity<BaseResponse<List<ScheduleResponse>>>
 
     @Operation(summary = "일별 일정 조회", description = "특정 날짜의 일정 목록을 조회합니다.")
     fun getSchedulesByDate(
         @Parameter(description = "조회할 날짜") @RequestParam date: LocalDate,
-    ): BaseResponse<List<DailyScheduleResponse>>
+    ): ResponseEntity<BaseResponse<List<DailyScheduleResponse>>>
 }
--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/schedule/presentation/ScheduleController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/schedule/presentation/ScheduleController.kt
@@ -9,6 +9,7 @@ import com.jininsadaecheonmyeong.starthubserver.global.common.BaseResponse
 import com.jininsadaecheonmyeong.starthubserver.global.security.token.support.UserAuthenticationHolder
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -27,34 +28,34 @@ class ScheduleController(
     @PostMapping
     override fun createSchedule(
         @RequestBody request: ScheduleRequest,
-    ): BaseResponse<Unit> {
+    ): ResponseEntity<BaseResponse<Unit>> {
         val user = userAuthenticationHolder.current()
         scheduleService.createSchedule(user.id!!, request)
-        return BaseResponse(Unit, HttpStatus.CREATED, "일정 생성 성공")
+        return BaseResponse.of(Unit, HttpStatus.CREATED, "일정 생성 성공")
     }
 
     @DeleteMapping
-    override fun deleteSchedule(@RequestParam announcementId: Long): BaseResponse<Unit> {
+    override fun deleteSchedule(@RequestParam announcementId: Long): ResponseEntity<BaseResponse<Unit>> {
         val user = userAuthenticationHolder.current()
         scheduleService.deleteSchedule(user.id!!, announcementId)
-        return BaseResponse(Unit, HttpStatus.OK, "일정 삭제 성공")
+        return BaseResponse.of(Unit, HttpStatus.OK, "일정 삭제 성공")
     }
 
     @GetMapping("/month")
     override fun getSchedulesByMonth(
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate,
-    ): BaseResponse<List<ScheduleResponse>> {
+    ): ResponseEntity<BaseResponse<List<ScheduleResponse>>> {
         val user = userAuthenticationHolder.current()
         val schedules = scheduleService.getSchedulesByMonth(user.id!!, date)
-        return BaseResponse(schedules, HttpStatus.OK, "월별 일정 조회 성공")
+        return BaseResponse.of(schedules, HttpStatus.OK, "월별 일정 조회 성공")
     }
 
     @GetMapping("/date")
     override fun getSchedulesByDate(
         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate,
-    ): BaseResponse<List<DailyScheduleResponse>> {
+    ): ResponseEntity<BaseResponse<List<DailyScheduleResponse>>> {
         val user = userAuthenticationHolder.current()
         val schedules = scheduleService.getSchedulesByDate(user.id!!, date)
-        return BaseResponse(schedules, HttpStatus.OK, "일별 일정 조회 성공")
+        return BaseResponse.of(schedules, HttpStatus.OK, "일별 일정 조회 성공")
     }
 }
