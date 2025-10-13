253401915ee0a6166d466af7b6f36a8c9c6fe4b4
bestdevmgp <me@mingyu.dev>
2025-10-13 18:05:52 +0900

    feat: fcm duplication condition

---
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/domain/notification/enums/DeviceType.kt
@@ -0,0 +1,9 @@
+package com.jininsadaecheonmyeong.starthubserver.domain.notification.enums
+
+enum class DeviceType(val displayName: String) {
+    ANDROID("Android"),
+    IOS("iOS"),
+    ANDROID_TABLET("Android Tablet"),
+    IPADOS("iPadOS"),
+    UNKNOWN("Unknown"),
+}
