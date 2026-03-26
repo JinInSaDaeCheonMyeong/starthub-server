708290ee162b5cbf2754a11eb2f918fbb231f917
bestdevmgp <me@mingyu.dev>
2026-03-26 15:33:07 +0900

    chore: ktlint

--- src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/presentation/controller/user/UserController.kt
+++ src/main/kotlin/com/jininsadaecheonmyeong/starthubserver/presentation/controller/user/UserController.kt
@@ -14,13 +14,13 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
